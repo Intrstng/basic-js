6416f9054098eecf84f015af1d5f9aeb5d25a305
Intrstng <intrstg@gmail.com>
2024-12-28 18:34:55 +0300

    feat: complete VigenereCipheringMachine task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.uppercaseAlphabet = Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i));
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    let result = '';
+    let keyIndex = 0;
+    let messageCapitalized = message.toUpperCase();
+    let keyCapitalized = key.toUpperCase();
+    const keyLength = keyCapitalized.length;
+
+    for (let i = 0; i < messageCapitalized.length; i += 1) {
+      const letter = messageCapitalized[i];
+      if (this.uppercaseAlphabet.includes(letter)) {
+        const messageCode = letter.charCodeAt(0) - 'A'.charCodeAt(0);
+        const keyCode = keyCapitalized[keyIndex % keyLength].charCodeAt(0) - 'A'.charCodeAt(0);
+        const encryptedChar = String.fromCharCode((messageCode + keyCode) % 26 + 'A'.charCodeAt(0));
+        result += encryptedChar;
+        keyIndex += 1;
+      } else {
+        result += letter;
+      }
+    }
+    if (this.direct) {
+      return result;
+    } else {
+      return result.split('').reverse().join('');
+    }
+  }
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+    let result = '';
+    let keyIndex = 0;
+    let encryptedMessageCapitalized = encryptedMessage.toUpperCase();
+    let keyCapitalized = key.toUpperCase();
+    const keyLength = keyCapitalized.length;
+
+    for (let i = 0; i < encryptedMessageCapitalized.length; i += 1) {
+      const letter = encryptedMessageCapitalized[i];
+      if (this.uppercaseAlphabet.includes(letter)) {
+        const encryptedCode = letter.charCodeAt(0) - 'A'.charCodeAt(0);
+        const keyCode = keyCapitalized[keyIndex % keyLength].charCodeAt(0) - 'A'.charCodeAt(0);
+        const decryptedChar = String.fromCharCode((encryptedCode - keyCode + 26) % 26 + 'A'.charCodeAt(0));
+        result += decryptedChar;
+        keyIndex += 1;
+      } else {
+        result += letter;
+      }
+    }
+    if (this.direct) {
+      return result;
+    } else {
+      return result.split('').reverse().join('');
+    }
   }
 }
 
